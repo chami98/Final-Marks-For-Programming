03aec3baac93e367d06ca70b121da0ea8ca6c195
chamikara <chamikara98@gmail.com>
2021-02-07 10:43:35 +0530

    Added label and buttons to the form

--- Final Marks For Programming/Form1.Designer.cs
+++ Final Marks For Programming/Form1.Designer.cs
@@ -35,12 +35,21 @@ namespace Final_Marks_For_Programming
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.studentName = new System.Windows.Forms.TextBox();
+            this.majorTest1 = new System.Windows.Forms.TextBox();
+            this.classTest = new System.Windows.Forms.TextBox();
+            this.examMark = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 22);
+            this.label1.Location = new System.Drawing.Point(13, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(98, 17);
             this.label1.TabIndex = 0;
@@ -50,7 +59,7 @@ namespace Final_Marks_For_Programming
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 85);
+            this.label2.Location = new System.Drawing.Point(12, 86);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(257, 17);
             this.label2.TabIndex = 1;
@@ -59,7 +68,7 @@ namespace Final_Marks_For_Programming
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(24, 137);
+            this.label3.Location = new System.Drawing.Point(33, 137);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(87, 17);
             this.label3.TabIndex = 2;
@@ -68,7 +77,7 @@ namespace Final_Marks_For_Programming
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(24, 178);
+            this.label4.Location = new System.Drawing.Point(33, 178);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(74, 17);
             this.label4.TabIndex = 3;
@@ -77,7 +86,7 @@ namespace Final_Marks_For_Programming
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(24, 217);
+            this.label5.Location = new System.Drawing.Point(34, 217);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(77, 17);
             this.label5.TabIndex = 4;
@@ -92,11 +101,91 @@ namespace Final_Marks_For_Programming
             this.label6.TabIndex = 5;
             this.label6.Text = "Major Test 2";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(270, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calculate Final Grade";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 283);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 17);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Final Mark";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(34, 333);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 17);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Grade";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(313, 327);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // studentName
+            // 
+            this.studentName.Location = new System.Drawing.Point(169, 22);
+            this.studentName.Name = "studentName";
+            this.studentName.Size = new System.Drawing.Size(242, 22);
+            this.studentName.TabIndex = 10;
+            // 
+            // majorTest1
+            // 
+            this.majorTest1.Location = new System.Drawing.Point(148, 137);
+            this.majorTest1.Name = "majorTest1";
+            this.majorTest1.Size = new System.Drawing.Size(50, 22);
+            this.majorTest1.TabIndex = 11;
+            // 
+            // classTest
+            // 
+            this.classTest.Location = new System.Drawing.Point(148, 178);
+            this.classTest.Name = "classTest";
+            this.classTest.Size = new System.Drawing.Size(50, 22);
+            this.classTest.TabIndex = 12;
+            // 
+            // examMark
+            // 
+            this.examMark.Location = new System.Drawing.Point(148, 217);
+            this.examMark.Name = "examMark";
+            this.examMark.Size = new System.Drawing.Size(50, 22);
+            this.examMark.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(396, 137);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(50, 22);
+            this.textBox1.TabIndex = 14;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(490, 456);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.examMark);
+            this.Controls.Add(this.classTest);
+            this.Controls.Add(this.majorTest1);
+            this.Controls.Add(this.studentName);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -118,6 +207,15 @@ namespace Final_Marks_For_Programming
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox studentName;
+        private System.Windows.Forms.TextBox majorTest1;
+        private System.Windows.Forms.TextBox classTest;
+        private System.Windows.Forms.TextBox examMark;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
